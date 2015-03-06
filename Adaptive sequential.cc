cdae0be461aef5bb46ca7e3ec4da1936b0cf9089
fredribh <freberhaa@hotmail.comgit config --global user.email freberhaa@hotmail.comgit config --global user.email freberhaa@hotmail.com>
2015-03-06 12:49:14 +0100

    Attempted fixing of adaptive prefetcher.

--- Adaptive sequential.cc
+++ Adaptive sequential.cc
@@ -1,47 +1,45 @@
 #include "interface.hh"
 
-unsigned short degree = 1;
-unsigned short total_prefetches = 0;
-unsigned short good_prefetches = 0;
-unsigned short counter = 5;
+unsigned int degree = 1;
+unsigned int prefetches = 0;
+unsigned int useful = 0;
+unsigned int counter = 10;
 
 void prefetch_init(void) {
-    
+	
 }
 
 void prefetch_access(AccessStat stat) {
-    Addr addr = stat.mem_addr;
+	Addr addr = stat.mem_addr;
 
-    if (get_prefetch_bit(addr)) {
-        good_prefetches++;
-        clear_prefetch_bit(addr);
-    }
+	if (get_prefetch_bit(addr)) {
+		useful++;
+		clear_prefetch_bit(addr);
+	}
 
-    if (total_prefetches == counter) {
-		if (good_prefetches >= 4) {
-			degree += 2;
-		}
-		else if (good_prefetches >= 2) {
+	if (prefetches == counter) {
+		if (useful > 6) {
 			degree += 1;
 		}
-		else if (good_prefetches == 0){
-			if (degree > 1)
+		else if(useful < 3) {
+			if (degree > 1) {
 				degree -= 1;
+			}
 		}
-        total_prefetches = 0;
-        good_prefetches = 0;
-    }
-    else {
-        total_prefetches++;
-    }
+		prefetches = 0;
+		useful = 0;
+	}
+	else {
+		prefetches++;
+	}
 
-    for (int i = 1; i <= degree; i++) {
-        addr += BLOCK_SIZE;
-        if (!in_cache(addr) && !in_mshr_queue(addr))
-            issue_prefetch(addr);
-    }
+	for (int i = 1; i <= degree; i++) {
+		addr += BLOCK_SIZE;
+		if (!in_cache(addr) && !in_mshr_queue(addr))
+			issue_prefetch(addr);
+	}
 }
 
 void prefetch_complete(Addr addr) {
-    set_prefetch_bit(addr);
+	set_prefetch_bit(addr);
 }
